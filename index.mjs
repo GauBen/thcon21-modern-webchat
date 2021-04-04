145fa119be3f3003be4b02cdee8a46896e958a50
GauBen <benaim.gautier@gmail.com>
2021-04-04 20:04:17 +0200

    feat: challenge

--- index.mjs
+++ index.mjs
@@ -2,36 +2,149 @@ import Koa from "koa";
 import fs from "fs";
 import ws from "ws";
 import http from "http";
+import koaStatic from "koa-static";
 
 const server = http.createServer();
 const app = new Koa();
 const wsServer = new ws.Server({ server: server, clientTracking: true });
-const index = fs.readFileSync("./index.html").toString();
 const flag = fs.readFileSync("./flag.txt").toString();
 
-app.use((ctx) => {
-  ctx.body = fs.readFileSync("./index.html").toString();
-});
+/** Nicknames in use. */
+const nicknames = new Set(["lepirebot", "info", "admin", "administrator"]);
+
+/** Send the flag to all administrators. */
+const sendFlag = () => {
+  wsServer.clients.forEach((client) => {
+    if (client.readyState !== client.OPEN) {
+      return;
+    }
+    if ("user" in client && "admin" in client.user && client.user.admin) {
+      client.send(
+        JSON.stringify({
+          user: { nickname: "LePireBot", color: "#ff4400", admin: true },
+          message: `Well done! Here is the flag: ${flag}`,
+        })
+      );
+    } else {
+      client.send(
+        JSON.stringify({
+          user: { nickname: "LePireBot", color: "#ff4400", admin: true },
+          message: "Message restricted to administrators.",
+        })
+      );
+    }
+  });
+};
+setInterval(sendFlag, 30000);
+
+/** Send an error message to a client. */
+const sendError = (client, message) => {
+  client.send(
+    JSON.stringify({
+      user: { nickname: "Error", color: "#ff4400" },
+      message: message,
+    })
+  );
+};
+
+/** Handle a data object sent to the server. */
+const handleData = (client, data) => {
+  if (!(data instanceof Object) || data.constructor !== Object) {
+    sendError(client, "Unexpected object received.");
+    return;
+  }
+  if ("user" in client) {
+    handleMessage(client, data);
+  } else {
+    handleLogin(client, data);
+  }
+};
+
+/** Handle a data object sent when the user is known. */
+const handleMessage = (client, data) => {
+  if (!("message" in data)) {
+    sendError(client, "Message not found.");
+    return;
+  }
+  const message = data.message.toString().substring(0, 280);
+  wsServer.clients.forEach((socket) => {
+    socket.send(
+      JSON.stringify({
+        user: client.user,
+        message: message,
+      })
+    );
+  });
+};
+
+/** Handle a login form. */
+const handleLogin = (client, data) => {
+  if (!("nickname" in data) || !/^[a-zA-Z0-9_.-]{3,20}$/.test(data.nickname)) {
+    sendError(client, "Nickname does not match the regular expression.");
+    return;
+  }
+  if (!("color" in data) || !/^#[0-9a-fA-F]{6}$/.test(data.color)) {
+    sendError(client, "Color does not match the regular expression.");
+    return;
+  }
+  if (nicknames.has(data.nickname.toString().toLowerCase())) {
+    sendError(client, "Nickname already in use.");
+    return;
+  }
+  if ("admin" in data) {
+    // Prevent hacking!
+    delete data.admin;
+  }
+  const user = Object.assign({}, data);
+  nicknames.add(user.nickname);
+  client.user = user;
+  client.send(
+    JSON.stringify({
+      user: { nickname: "Info", color: "#0088ff" },
+      message: `Welcome ${user.nickname}! ${wsServer.clients.size} people connected.`,
+      successfulLogin: true,
+    })
+  );
+  wsServer.clients.forEach((socket) => {
+    if (socket === client) {
+      return;
+    }
+    socket.send(
+      JSON.stringify({
+        user: { nickname: "Info", color: "#0088ff" },
+        message: `${user.nickname} joined the chat room! ${wsServer.clients.size} people connected.`,
+      })
+    );
+  });
+};
 
 wsServer.on("connection", (client) => {
+  client.send(
+    '{"user":{"nickname":"Info","color":"#0088ff"},"message":"Connected to the chat room."}'
+  );
+
   client.on("message", (message) => {
     let data;
     try {
       data = JSON.parse(message);
     } catch {
-      client.send('{"error": "SyntaxError in JSON"}');
+      sendError(client, "SyntaxError in JSON object sent.");
       return;
     }
+    handleData(client, data);
+  });
 
-    if ("user" in client) {
-      console.log(client.user);
-    } else {
-      client.user = data;
-      console.log(client.user);
+  client.on("close", () => {
+    if (
+      "user" in client &&
+      "nickname" in client.user &&
+      nicknames.has(client.user.toString().toLowerCase())
+    ) {
+      nicknames.delete(client.user.toString().toLowerCase());
     }
-    client.send(JSON.stringify(client.user));
   });
 });
 
+app.use(koaStatic("./public"));
 server.on("request", app.callback());
 server.listen(8080);
---
+++ public/main.js
@@ -0,0 +1,59 @@
+const escape = (string) =>
+  string.replace(/[&<>"']/g, (chr) => {
+    return {
+      "&": "&amp;",
+      "<": "&lt;",
+      ">": "&gt;",
+      '"': "&quot;",
+      "'": "&#39;",
+    }[chr];
+  });
+
+const ws = new WebSocket("ws://" + location.host);
+
+const $loginForm = document.querySelector("#login-form");
+const $messageForm = document.querySelector("#message-form");
+const $messages = document.querySelector("#messages");
+
+const createMessage = (data) => {
+  const $el = document.createElement("div");
+  $el.className = "message";
+  $el.innerHTML = `<strong style="color:${data.user.color}">${escape(
+    escape(data.user.nickname)
+  )}</strong>: ${escape(data.message)}`;
+  return $el;
+};
+
+ws.onmessage = (e) => {
+  const shouldScroll =
+    $messages.scrollTop + $messages.clientHeight === $messages.scrollHeight;
+  const data = JSON.parse(e.data);
+  $messages.appendChild(createMessage(data));
+  if (shouldScroll) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+  if ("successfulLogin" in data) {
+    $loginForm.hidden = true;
+    $messageForm.hidden = false;
+  }
+};
+
+$loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  ws.send(
+    JSON.stringify({
+      nickname: $loginForm.elements.nickname.value,
+      color: $loginForm.elements.color.value,
+    })
+  );
+});
+
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  ws.send(
+    JSON.stringify({
+      message: $messageForm.elements.message.value,
+    })
+  );
+  $messageForm.elements.message.value = "";
+});
